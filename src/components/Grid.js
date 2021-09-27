7ec2899ec0efce56db1bc1db5d023347f91898fb
Marge <marge.consunji@gmail.com>
2021-09-27 12:51:40 +0800

    Made Grid component, file for compiling all component exports

---
+++ src/components/Grid.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Grid = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+`;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { Grid } from "./Grid";
+export { Header } from "./Header";
