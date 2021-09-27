62fe567c4b1ebb8ff4540428e1cb15f3f65ef3ba
Marge <marge.consunji@gmail.com>
2021-09-27 16:57:39 +0800

    Added Background component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Overview } from "./Overview";
 export { Grid } from "./Grid";
 export { Header } from "./Header";
 export { Platforms } from "./Platforms";
+export { Background } from "./Background";
