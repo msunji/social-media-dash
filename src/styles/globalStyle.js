4a09a3443d1fcdb1f7244b950177b91fd85a0ba9
Marge <marge.consunji@gmail.com>
2021-09-27 16:56:46 +0800

    Added antialiasing

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -6,8 +6,17 @@ const GlobalStyle = createGlobalStyle`
 
   :root {
     --sans-serif: "Inter", sans-serif;
+    --limeGreen: hsl(163, 72%, 41%);
+    --brightRed: hsl(356, 69%, 56%);
+    --facebook: hsl(208, 92%, 53%);
+    --twitter: hsl(203, 89%, 53%);
+    --youtube: hsl(348, 97%, 39%);
+    --instagram: linear-gradient(36deg, rgba(253,196,104,1) 0%, rgba(223,73,150,1) 98%);
   }
-
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
   *,
   *:before,
   *:after {
@@ -20,6 +29,11 @@ const GlobalStyle = createGlobalStyle`
     overflow-x: hidden;
     font-size: 14px;
     font-family: var(--sans-serif);
+  }
+
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.textMinor}
   }
 `;
 
