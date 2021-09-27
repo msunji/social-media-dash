76f6477925ef40453f8bc91cb64d8584a7b1bf2a
Marge <marge.consunji@gmail.com>
2021-09-27 16:56:58 +0800

    Added breakpoints object

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -11,8 +11,11 @@ export const lightTheme = {
 export const darkTheme = {
   background: "hsl(230, 17%, 14%)",
   textMajor: "hsl(0, 0%, 100%)",
-  textMinor: "hsl(228, 12%, 44%)",
+  textMinor: "hsl(228, 34%, 66%)",
   topBg: "hsl(232, 19%, 15%)",
   cardBg: "hsl(228, 28%, 20%)",
 };
-// const breakpoints = {};
+
+export const breakpoints = {
+  lg: "(min-width: 700px)",
+};
