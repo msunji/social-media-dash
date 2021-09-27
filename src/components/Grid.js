c645c8746b2cf7dae617134a71916d0784bcd500
Marge <marge.consunji@gmail.com>
2021-09-27 21:36:48 +0800

    Made grid narrower, set max-width to 1110px

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Grid = styled.main`
   display: grid;
   grid-template-columns: 1fr;
-  width: 90%;
+  width: 85%;
+  max-width: 1110px;
   margin-left: auto;
   margin-right: auto;
   padding-top: 3rem;
