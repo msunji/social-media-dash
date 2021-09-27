9feb6ec27be9cd40fb4196a697e1fdeb28e787c3
Marge <marge.consunji@gmail.com>
2021-09-27 18:37:01 +0800

    Added simple dark toggle - still needs styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./styles/constants";
+import { lightTheme, darkTheme } from "./styles/constants";
 import GlobalStyle from "./styles/globalStyle";
 import { Grid, Header, Overview, Platforms, Background } from "./components/";
 import Data from "./content/data.json";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleThemeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
-      <ThemeProvider theme={lightTheme}>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
         <GlobalStyle />
         <Background />
         <Grid>
-          <Header data={Data} />
+          <Header data={Data} toggle={handleThemeToggle} />
           <Platforms data={Data} />
           <Overview data={Data} />
         </Grid>
