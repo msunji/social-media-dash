d71208a0f8f9f071caab15b32dd7ffcd25200c68
Marge <marge.consunji@gmail.com>
2021-09-27 21:11:54 +0800

    Made custom dark mode hook

---
+++ src/utils/useDarkMode.js
@@ -0,0 +1,32 @@
+// A hook for implementing dark mode
+import { useState, useEffect } from "react";
+
+export const useDarkMode = () => {
+  const [theme, setTheme] = useState("light");
+  const [mounted, setMounted] = useState(false);
+
+  const setThemeType = (type) => {
+    window.localStorage.setItem("theme", type);
+    setTheme(type);
+  };
+
+  const handleThemeToggle = () => {
+    if (theme === "light") {
+      setThemeType("dark");
+    } else {
+      setThemeType("light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      setThemeType("light");
+    }
+    setMounted(true);
+  }, []);
+
+  return [theme, handleThemeToggle, mounted];
+};
