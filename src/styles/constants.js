e319202f92026b48b5e6dbfcba151b2a5a8de6fd
Marge <marge.consunji@gmail.com>
2021-09-27 19:14:27 +0800

    Added larger breakpoint value

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -7,6 +7,7 @@ export const lightTheme = {
   topBg: "hsl(225, 100%, 98%)",
   cardBg: "hsl(227, 47%, 96%)",
   cardHover: "hsl(228, 33%, 91%)",
+  toggle: "hsl(230, 22%, 74%)",
 };
 
 export const darkTheme = {
@@ -16,8 +17,10 @@ export const darkTheme = {
   topBg: "hsl(232, 19%, 15%)",
   cardBg: "hsl(228, 28%, 20%)",
   cardHover: "hsl(228, 25%, 27%)",
+  toggle: "linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))",
 };
 
 export const breakpoints = {
   lg: "(min-width: 700px)",
+  xl: "(min-width: 1000px)",
 };
