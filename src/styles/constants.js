8131969b231cccf56071fe9376bbcc6584e36975
Marge <marge.consunji@gmail.com>
2021-09-27 13:04:21 +0800

    Set colours for soc media and light theme

---
+++ src/styles/constants.js
@@ -0,0 +1,25 @@
+// Theme colours
+export const theme = {
+  base: {
+    colours: {
+      limeGreen: "hsl(163, 72%, 41%)",
+      brightRed: "hsl(356, 69%, 56%)",
+      facebook: "hsl(208, 92%, 53%)",
+      twitter: "hsl(203, 89%, 53%)",
+      instagram:
+        "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))",
+    },
+  },
+  light: {
+    colours: {
+      white: "hsl(0, 0%, 100%)",
+      veryPaleBlue: "hsl(225, 100%, 98%)",
+      lightGreyBlue: "hsl(227, 47%, 96%)",
+      darkGreyBlue: "hsl(228, 12%, 44%)",
+      veryDarkBlue: "hsl(230, 17%, 14%)",
+    },
+  },
+  // dark: {},
+};
+
+// const breakpoints = {};
