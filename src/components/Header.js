57fbf6a41c0e7dec4d3e6f4326e81819aa93fff4
Marge <marge.consunji@gmail.com>
2021-09-27 16:45:14 +0800

    Added top and bottom margins

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
+import { handleNumSeparator } from "../utils/helpers";
 
-export const Header = (props) => {
-  return <div></div>;
+const HeaderSection = styled.section`
+  font-weight: 700;
+  margin-bottom: 3rem;
+  h1 {
+    font-size: 1.71em;
+    color: ${({ theme }) => theme.textMajor};
+    margin-bottom: 0.8rem;
+  }
+  p {
+    color: ${({ theme }) => theme.textMinor};
+  }
+`;
+
+export const Header = ({ data }) => {
+  const totalFollowers = data.followers[0].total;
+
+  return (
+    <HeaderSection>
+      <h1>Social Media Dashboard</h1>
+      <p>Total Followers: {handleNumSeparator(totalFollowers)}</p>
+    </HeaderSection>
+  );
 };
